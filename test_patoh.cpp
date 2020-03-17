25f12863ffbd52025b8360d629d98df3a1ac0eaf
Lars Gottesbüren <lars.gottesbueren@kit.edu>
2020-03-17 21:39:30 +0100

    make test_patoh.cpp output in the format used for our experiments

--- test_patoh.cpp
+++ test_patoh.cpp
@@ -10,23 +10,39 @@ int main(int argc, const char *argv[]) {
     if (argc != 6) {
         throw std::runtime_error("Usage ./PaToH HypergraphFile epsilon k seed preset");
     }
-    whfc_rb::CSRHypergraph hg = whfc::HMetisIO::readCSRHypergraph(argv[1]);
+    std::string graph_path = argv[1];
     double epsilon = std::stod(argv[2]);
     uint numParts = std::stoul(argv[3]);
     int seed = std::stoi(argv[4]);
     std::string patoh_preset = argv[5];
 
-
+    whfc_rb::CSRHypergraph hg = whfc::HMetisIO::readCSRHypergraph(graph_path);
+    
     whfc::TimeReporter timer("Total");
     whfc_rb::PartitionBase partition(numParts, hg);
 
     timer.start("Total");
     PaToHInterface::partitionWithPatoh(partition, seed, numParts, epsilon, patoh_preset);
     timer.stop("Total");
-    timer.report(std::cout);
+//    timer.report(std::cout);
     //partition.print(std::cout);
-    std::cout << "Imbalance: " << partition.imbalance() << std::endl;
-    std::cout << "Num parts: " << partition.numParts() << std::endl;
-    std::cout << "km1: " << partition.km1Objective() << std::endl;
-    std::cout << "cut: " << partition.cutObjective() << std::endl;
+
+    std::string graph_name = graph_path.substr(graph_path.find_last_of('/') + 1);
+    std::cout   << "PaToH-" << patoh_preset << ","
+                << graph_name << ","
+                << "no" << ","
+                << seed << ","
+                << partition.numParts() << ","
+                << epsilon << ","
+                << partition.imbalance() << ","
+                << whfc::inSeconds(timer.get("Total")).count() << ","
+                << "km1" << ","
+                << partition.km1Objective() << ","
+                << partition.cutObjective() << ","
+                << "no" << std::endl;
+
+//    std::cout << "Imbalance: " << partition.imbalance() << std::endl;
+//    std::cout << "Num parts: " << partition.numParts() << std::endl;
+//    std::cout << "km1: " << partition.km1Objective() << std::endl;
+//    std::cout << "cut: " << partition.cutObjective() << std::endl;
 }
