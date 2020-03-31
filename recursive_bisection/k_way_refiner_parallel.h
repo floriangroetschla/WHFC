c742a9bc252e4cd4fe28ce25682b33341158a7a6
Lars Gottesbüren <lars.gottesbueren@kit.edu>
2020-03-31 14:32:17 +0200

    check block pair status before trying to acquire the part. in order to reduce write contention

--- recursive_bisection/k_way_refiner_parallel.h
+++ recursive_bisection/k_way_refiner_parallel.h
@@ -135,7 +135,8 @@ namespace whfc_rb {
         bool addNewTasks(PartitionID part, tbb::parallel_do_feeder<WorkElement>& feeder, NodeWeight maxWeight) {
             bool foundPair = false;
             for (PartitionID pid = 0; pid < partition.numParts(); ++pid) {
-                if (pid != part && guessNumCutEdges(part, pid) > 0 && !partScheduled[pid]) {
+                if (pid != part && guessNumCutEdges(part, pid) > 0 && !partScheduled[pid]
+                        && blockPairStatus(part, pid) == TaskStatus::UNSCHEDULED) {
                     if (!partScheduled[pid].exchange(true)) {
                         if (blockPairStatus(part, pid) == TaskStatus::UNSCHEDULED) {
                             blockPairStatus(part, pid) = TaskStatus::SCHEDULED;
