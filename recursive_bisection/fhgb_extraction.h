ca4dfa56ef8b018050ae0ed11efee58c943eb454
Florian Grötschla <florian.groetschla@student.kit.edu>
2020-04-23 11:16:36 +0200

    Mock builder

--- recursive_bisection/fhgb_extraction.h
+++ recursive_bisection/fhgb_extraction.h
@@ -11,6 +11,86 @@
 namespace whfc_rb {
     class FlowHypergraphBuilderExtractor {
     public:
+        class MockBuilder {
+        public:
+            using Flow = whfc::Flow;
+            using Node = whfc::Node;
+            using NodeData = whfc::FlowHypergraph::NodeData;
+            using HyperedgeData = whfc::FlowHypergraph::HyperedgeData;
+            using Pin = whfc::FlowHypergraph::Pin;
+            using InHeIndex = whfc::InHeIndex;
+            using PinIndex = whfc::PinIndex;
+
+            MockBuilder() {
+                clear();
+            }
+
+            void clear() {
+                nodes.clear();
+                hyperedges.clear();
+                pins.clear();
+
+                numPinsAtHyperedgeStart = 0;
+                maxHyperedgeCapacity = 0;
+
+                nodes.push_back({InHeIndex(0), NodeWeight(0)});
+                hyperedges.push_back({PinIndex(0), Flow(0), Flow(0)});
+            }
+
+            void startHyperedge(const Flow capacity) {
+                finishHyperedge();	//finish last hyperedge
+                hyperedges.back().capacity = capacity;	//exploit sentinel
+                numPinsAtHyperedgeStart = numPins();
+                maxHyperedgeCapacity = std::max(maxHyperedgeCapacity, capacity);
+            }
+
+            void addPin(const Node u) {
+                assert(u < numNodes());
+                pins.push_back(u);
+                nodes[u+1].first_out++;
+            }
+
+            void addNode(const NodeWeight w) {
+                nodes.back().weight = w;
+                nodes.push_back({InHeIndex(0), NodeWeight(0)});
+            }
+
+            inline size_t numPins() const { return pins.size(); }
+
+            inline size_t numNodes() const { return nodes.size() - 1 ; }
+
+            size_t currentHyperedgeSize() const {
+                return numPins() - numPinsAtHyperedgeStart;
+            }
+
+        private:
+            std::vector<NodeData> nodes;
+            std::vector<HyperedgeData> hyperedges;
+            std::vector<Node> pins;
+
+            size_t numPinsAtHyperedgeStart = 0;
+            Flow maxHyperedgeCapacity = 0;
+
+            void removeLastPin() {
+                nodes[ pins.back() + 1 ].first_out--;
+                pins.pop_back();
+            }
+
+            bool finishHyperedge() {
+                if (currentHyperedgeSize() == 1) {
+                    removeLastPin();
+                }
+
+                if (currentHyperedgeSize() > 0) {
+                    //pins_sending_flow.emplace_back(hyperedges.back().first_out, hyperedges.back().first_out);
+                    hyperedges.push_back({PinIndex::fromOtherValueType(numPins()), Flow(0), Flow(0)});//sentinel
+                    //pins_receiving_flow.emplace_back(hyperedges.back().first_out, hyperedges.back().first_out);
+                    return true;
+                }
+                return false;
+            }
+        };
+
         static constexpr NodeID invalid_node = std::numeric_limits<NodeID>::max();
         whfc::FlowHypergraphBuilder fhgb;
 
@@ -111,11 +191,11 @@ namespace whfc_rb {
             w += hg.nodeWeight(node);
         }
 
-        template<class PartitionImpl, typename CutEdgeRange>
+        template<class PartitionImpl, typename CutEdgeRange, typename Builder>
         whfc::NodeWeight BreadthFirstSearch(CSRHypergraph &hg, CutEdgeRange &cut_hes, const PartitionImpl &partition,
                                             PartitionBase::PartitionID partID, PartitionBase::PartitionID otherPartID,
                                             NodeWeight maxWeight, whfc::Node terminal, whfc::HopDistance delta,
-                                            whfc::DistanceFromCut& distanceFromCut, whfc::TimeReporter& timer) {
+                                            whfc::DistanceFromCut& distanceFromCut, whfc::TimeReporter& timer, Builder& builder) {
             whfc::NodeWeight w = 0;
             whfc::HopDistance d = delta;
 
@@ -169,7 +249,6 @@ namespace whfc_rb {
                         }
                         visitedHyperedge.add(e);
                     }
-
                 }
             }
 
